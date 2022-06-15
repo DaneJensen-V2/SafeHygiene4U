656625bc5918b8fde0680ab90345a32fa76cef4b
DaneJensen-V2 <dejense4@asu.edu>
2022-06-15 15:20:26 -0700

    Initial Commit

---
+++ Safe Hygeine 4U/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Safe Hygeine 4U
+//
+//  Created by Dane Jensen on 6/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
