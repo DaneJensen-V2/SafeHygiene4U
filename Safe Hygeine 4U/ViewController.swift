dbb00219121c3dbe03e33663b63ae285cd8587da
DaneJensen-V2 <dejense4@asu.edu>
2022-06-15 15:48:40 -0700

    First push test

--- Safe Hygeine 4U/ViewController.swift
+++ Safe Hygeine 4U/ViewController.swift
@@ -12,8 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //first push test
     }
-//test
+
 
 }
 
